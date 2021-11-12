aebc81e128547da535f6cdab9875eea8cfd83480
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-11-12 13:52:11 -0800

    making things move

---
+++ circles.py
@@ -0,0 +1,81 @@
+import pygame
+pygame.init()
+
+#configure the screen
+screen = pygame.display.set_mode([500, 500])
+w, h = pygame.display.get_surface().get_size()
+
+def draw_color_circles():
+  blue = (20, 121, 199)
+  red = (255, 0, 0)
+  orange = (250, 141, 7)
+  yellow = (255, 255, 0)
+  green = (25, 184, 22)
+
+  radius = 50
+
+  top_left = (0 + radius, 0 + radius)
+  top_right = (w - radius, radius)
+  bottom_left = (radius, h - radius)
+  bottom_right = (w - radius, h - radius)
+  center = (w / 2, h / 2)
+
+  pygame.draw.circle(screen, red, top_left, radius)
+  pygame.draw.circle(screen, orange, top_right, radius)
+  pygame.draw.circle(screen, yellow, center, radius)
+  pygame.draw.circle(screen, green, bottom_left, radius)
+  pygame.draw.circle(screen, blue, bottom_right, radius)
+
+def draw_circle_grid():
+  grey = (100, 100, 100)
+  radius = 50
+  for i in range(3):
+    for j in range(3):
+      pygame.draw.circle(screen, grey, (w * (i / 3) + radius, h * ((j % 3) / 3) + radius), radius)
+
+""" 
+0, 0
+0, 250
+0, 500
+250, 0
+250, 250
+250, 500
+
+0 / 3 = 0
+1 / 3 = 0.33
+2 / 3 = 0.66
+3 / 3 = 1
+4 / 3 = 1.33
+5 / 3 = 1.66
+
+0 % 3 = 0
+1 % 3 = 1
+2 % 3 = 2
+3 % 3 = 0
+4 % 3 = 1
+5 % 3 = 2
+
+500 / 1 / 3
+ """
+
+#game loop
+running = True
+while running: 
+  #look at events
+  for event in pygame.event.get():
+    # close game window
+    if event.type == pygame.QUIT:
+      running = False
+
+  #clear screen
+  black = (0, 0, 0)
+  screen.fill(black)
+  #draw_color_circles()
+
+  draw_circle_grid()
+
+  #update the display
+  pygame.display.flip()
+
+  
+
--- game.py
+++ game.py
@@ -1,49 +1,167 @@
-# Import and initialize pygame
 import pygame
 pygame.init()
-# Configure the screen
+from random import randint, choice
+
+clock = pygame.time.Clock()
+
+#configure the screen
 screen = pygame.display.set_mode([500, 500])
-# Creat the game loop
-running = True
+w, h = pygame.display.get_surface().get_size()
 
 class GameObject(pygame.sprite.Sprite):
-  # Remove width and height and add image here!
   def __init__(self, x, y, image):
     super(GameObject, self).__init__()
-    # self.surf = pygame.Surface((width, height)) REMOVE!
-    # self.surf.fill((255, 0, 255)) REMOVE!
-    self.surf = pygame.image.load(image) # ADD!
+
+    self.surf = pygame.image.load(image)
+    self.rect = self.surf.get_rect()
     self.x = x
     self.y = y
-
+    self.rect = self.surf.get_rect()
+  
   def render(self, screen):
+    self.rect.x = self.x
+    self.rect.y = self.y
     screen.blit(self.surf, (self.x, self.y))
 
-#box = GameObject(120, 300, 50, 50)
-apple = GameObject(120, 300, 'apple.png') # ADD!
-apple.render(screen) # ADD!
+apple2 = GameObject(w / 2, 0, './images/apple.png')
 
+
+#3 Columns & Rows
+center = w / 2
+middle = center - 32
+left = center / 2 - 25
+right =  w - (center / 2) - 25
+
+x_lanes = [left, middle, right]
+
+center_horizontal = h / 2
+center_horizontal_lane = center_horizontal - 32
+top = center_horizontal / 2 - 25
+bottom = h - (center_horizontal / 2) - 25
+
+y_lanes = [top, center_horizontal_lane, bottom]
+
+class Apple(GameObject):
+  def __init__(self):
+    self.x = choice(x_lanes)
+    super(Apple, self).__init__(0, 0, './images/apple.png')
+    self.dx = 0
+    self.dy = (randint(0, 200) / 100) + 1
+    self.reset()
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.y > h:
+      self.reset()
+
+  def reset(self):
+    self.x = choice(x_lanes)
+    self.y = -64
+
+
+class Strawberry(GameObject):
+  def __init__(self):
+    self.y = choice(y_lanes)
+    super(Strawberry, self).__init__(0, 0, './images/strawberry.png')
+    self.dy = 0
+    self.dx = (randint(0, 200) / 100) + 1
+    self.reset()
+
+  def move(self):
+    self.y += self.dy
+    self.x += self.dx
+    if self.x > w:
+      self.reset()
+  
+  def reset(self):
+    self.y = choice(y_lanes)
+    self.x = -64
+
+apple = Apple()
+strawberry = Strawberry()
+
+#player
+class Player(GameObject):
+  def __init__(self):
+    super(Player, self).__init__(0, 0, './images/player.png')
+    self.dx = 0
+    self.dy = 0
+    self.pos_x = 1
+    self.pos_y = 1
+    self.reset()
+
+  def left(self):
+    if self.pos_x > 0:
+      self.pos_x -= 1
+      self.update_dx_dy()
+
+  def right(self):
+    if self.pos_x < len(x_lanes) - 1:
+      self.pos_x += 1
+      self.update_dx_dy()
+
+  def up(self):
+    if self.pos_y > 0:
+      self.pos_y -= 1
+      self.update_dx_dy()
+
+  def down(self): 
+    if self.pos_y < len(y_lanes) - 1:
+      self.pos_y += 1
+      self.update_dx_dy()
+
+  def move(self):
+    self.x -= (self.x - self.dx) * 0.25
+    self.y -= (self.y - self.dy) * 0.25
+
+  def reset(self):
+    self.x = x_lanes[self.pos_x]
+    self.y = y_lanes[self.pos_y]
+    self.dx = self.x
+    self.dy = self.y
+
+  def update_dx_dy(self):
+    self.dx = x_lanes[self.pos_x]
+    self.dy = y_lanes[self.pos_y]
+
+player = Player()
+
+#make a group
+all_sprites = pygame.sprite.Group()
+
+all_sprites.add(player)
+all_sprites.add(apple)
+all_sprites.add(strawberry)
+
+#game loop
+running = True
 while running:
-  # Looks at events
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_ESCAPE:
+        running = False
+      elif event.key == pygame.K_LEFT:
+        player.left()
+      elif event.key == pygame.K_RIGHT:
+        player.right()
+      elif event.key == pygame.K_UP:
+        player.up()
+      elif event.key == pygame.K_DOWN:
+        player.down()
       
-  # Draw a circle
-  screen.fill((255, 255, 255))
+  fill_color = (0, 0, 0)
+  screen.fill(fill_color)
 
-  box.render(screen)
-  # Update the window
+  for entity in all_sprites:
+    entity.move()
+    entity.render(screen)
+
+  #update window
   pygame.display.flip()
-
-  # Configure the screen
-screen = pygame.display.set_mode([500, 500])
-
-# Clear screen
-screen.fill((255, 255, 255))
-
-def render(self, screen):
-  screen.blit(self.surf, (self.x, self.y))
-
-
+  #clock
+  clock.tick(60)
 
